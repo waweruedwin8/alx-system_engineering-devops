047315924ef684eeb05aa5dc6899eacb1ead7cc3
waweruedwin8 <waweruedwin8@gmail.com>
2024-05-20 18:49:05 +0300

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,67 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""
+2-export_to_JSON.py
+
+This script fetches and exports TODO list progress for a given employee ID
+from a REST API in JSON format.
+"""
+
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+def fetch_employee_data(employee_id):
+    """
+    Fetches employee data and TODO list progress from the REST API.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        tuple: A tuple containing the employee username and a list of tasks.
+    """
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200:
+        print("User not found")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    return user_data['username'], todos_data
+
+
+def export_to_json(employee_id, username, todos):
+    """
+    Exports the TODO list progress to a JSON file.
+
+    Args:
+        employee_id (int): The ID of the employee.
+        username (str): The username of the employee.
+        todos (list): A list of tasks.
+    """
+    filename = f"{employee_id}.json"
+    data = {str(employee_id): [{"task": todo['title'], "completed": todo['completed'], "username": username} for todo in todos]}
+    with open(filename, mode='w') as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    username, todos = fetch_employee_data(employee_id)
+    export_to_json(employee_id, username, todos)
